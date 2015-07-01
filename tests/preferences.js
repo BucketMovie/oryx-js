89a8e02e734af86e53dfb0688f519ffd12563e36
cesarBere <c.berezowski@gmail.com>
2015-07-01 16:33:48 +0200

    Patched delete pref function (used to POST instead of DELETE)

--- tests/preferences.js
+++ tests/preferences.js
@@ -1,15 +1,17 @@
 'use strict';
 
 var options = {
-    host: 'localhost',
-    port: '8091',
+    host: '176.31.249.9',
+    port: '8093',
     user: null,
     password: null
 };
 
-var assert = require('assert');
+var assert = require('assert'),
+    Q = require('q');
 
-var OryxService = require('../lib/index.js')(options);
+var OryxService = require('../lib/index.js')(options),
+    setPref = Q.denodeify(OryxService.preference.set);
 
 describe('preferences', function () {
     it('should set preference for user 1 and item 404', function (done) {
@@ -28,9 +30,9 @@ describe('preferences', function () {
         });
     });
     it('should set empty preference for user 1 and item 404', function (done) {
-        OryxService.preference.set(1, 404, function (err, res, body) {
-            assert(!err);
-            assert.equal(body, "");
+        setPref(1, 404).then(function (res) {
+            assert.equal(res[0].body, "");
+            assert.equal(res[0].statusCode, 200)
             done();
         });
     });
